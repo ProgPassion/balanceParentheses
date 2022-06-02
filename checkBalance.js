e48ba7feffa3a3d08ec2a0061f1f597eea444079
imran <31067252+ProgPassion@users.noreply.github.com>
2022-06-02 18:16:20 +0200

    Create checkBalance.js

---
+++ checkBalance.js
@@ -0,0 +1,18 @@
+var string = "((())(()))((()))";
+
+function checkBalance(string, position) {
+    if(string.length == 0) {
+        return true;   
+    }
+    
+	if(string[position] == "(" && string[position+1] == ")") {
+        string.splice(position, 2);
+        return checkBalance(string, 0);
+    }
+    if(position < string.length - 1)
+        return checkBalance(string, position + 1);
+    else 
+        return false;
+}
+
+checkBalance(string.split(''), 0);
